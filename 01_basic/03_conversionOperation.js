f2b1df6dd182d63410fc4a6f3dfd3b8f46cd17a4
kundankrishna8084 <81030641+kundankrishna8084@users.noreply.github.com>
2025-01-06 13:19:59 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,56 @@
+/*
+let score="33"
+console.log(typeof score); // First way to check type of datatypes
+
+let score2=89
+console.log(typeof (score2)); // Second way to check type of dataTypes
+
+
+//let score1="330abc"// NaN
+//let score1=null; // 0
+//let score1= undefined // Nan
+ // let score1=true ==>1
+let score1=false // ==>0
+
+console.log(typeof (score));
+
+
+let valueInNumber=Number(score1)
+
+console.log(typeof (score1));
+console.log(valueInNumber);
+
+
+
+// "33"=> 33
+// "33abc" ==> NaN(Not a number)
+// true =1
+// false=0
+// null==>0
+// undefined==> NaN
+
+
+// let isLoggedIn=1  ==>true
+//let isLoggedIn=0  ==>false
+//let isLoggedIn="" // ==>false
+let isLoggedIn="Kundan" // ==> true
+console.log(typeof (isLoggedIn));
+
+let booleanLoggedIn=Boolean(isLoggedIn)
+console.log (booleanLoggedIn);
+
+// String conversion
+
+let somenumber=33
+console.log(typeof (somenumber));
+
+let stringNumber= String(somenumber)
+console.log(stringNumber);
+console.log(typeof (stringNumber));
+
+*/
+
+
+// ========== Opeartons==============
+
+
