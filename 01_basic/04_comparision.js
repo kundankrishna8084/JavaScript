b2a410fbf770e0df0f8dec0ed394a20037c3aab3
kundankrishna8084 <81030641+kundankrishna8084@users.noreply.github.com>
2025-01-07 13:29:12 +0000

    comparision of datattypes

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,36 @@
+// console.log(2>3);
+// console.log(2<3);
+// console.log(2>=3);
+// console.log(2==2);
+// console.log(2!=3);
+
+
+
+
+
+ // Avoid these type of converision.
+ 
+// console.log("2">1);
+// console.log("02">1);
+
+
+
+
+
+console.log(null>0);  // ==>false
+console.log(null<0); // ==>false
+console.log(null==0); // ==>false
+console.log(null>=0); // ==> true because value covert in zero then true
+
+
+
+console.log(undefined>0); // ==>false
+console.log(undefined<0);// ==>false
+console.log(undefined==0);// ==>false
+console.log(undefined>=0);// ==>false
+
+
+// ===
+console.log("2"==2);
+console.log("2"===2);
+
