71c02f4bc6007ddbe09c686265124cd34cbba4bf
kundankrishna8084 <81030641+kundankrishna8084@users.noreply.github.com>
2025-01-19 12:42:35 +0000

    Types of Variable

---
+++ 01_basic/06_typesOfVariable.js
@@ -0,0 +1,50 @@
+//! Types of variable and its scope
+//? Global varaible
+//?Local variable
+
+//! Global Varaible
+// var firstname = "john";
+// let lastname = "doe";
+// const salary = 50000;
+
+//  if (10 > 2) {
+// console.log(firstname, lastname, salary);
+// }
+
+// for (let i = 0; i < 5; i++) {
+// console.log(firstname, lastname, salary);
+// }
+
+//  function greet(){
+// console.log(firstname, lastname, salary);
+// }
+// greet()
+
+
+
+
+//! local variable
+// if(10>2){
+//     var a=10;
+//     let b=20;
+//     const c=30;
+//     console.log(a,b,c);
+// }
+// console.log(a);
+
+//console.log(b);
+//console.log(c);
+
+
+
+
+// function test(){
+//     var x=100;
+//     let y =200;
+//     const z=300;
+//     console.log(x,y,z);   
+
+// }
+// test();
+
+//console.log(x);//! No reference error x is not defined.
