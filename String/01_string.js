627d34cd278b9a21d0427f930bf60e252e2f9024
kundankrishna8084 <81030641+kundankrishna8084@users.noreply.github.com>
2025-01-22 05:00:05 +0000

    String

---
+++ String/01_string.js
@@ -0,0 +1,33 @@
+//! STRING 
+    // //Single quote ' '
+    // var str1='Hello world'
+    // console.log(str1);
+    
+    // console.log(str1+" "+"Hii");
+    
+    
+    // //Double quote " "
+    // var str2 ="Hello Universe"
+    // console.log(str2);
+    // console.log(str2+" " +"Hii");
+    
+    
+    // //backticks  ` `
+    // // It accepts multi-line String 
+    // var str3=`Hello galaxy
+    // Hello Everyone`
+    // console.log(str3);
+
+    // //String interpolation ${}
+    // var str4 = `All String are ${str1} ${str2} ${str3}`
+    // console.log(str4);
+    
+    
+    //!STRING PROPERTY
+    // var str1="javaScript"
+    // console.log(str1);//javaScript
+    // console.log(str1[0]);//j
+    // console.log(str1[5]);//c
+    // console.log(str1.length);//10
+    // console.log(str1[str1.length-1]);//t
+    
